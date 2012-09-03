fb2467ff40fa0ab2f274e4469f78f61afb7459c9
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-09-03 14:56:33 +0200

    nebulae

--- sav.py
+++ sav.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python
 
+from utils import *
+
 class Player:
 	def __init__(self, binary, debug=False):
 		self.debug = debug
 
-		self.name = binary[175:195]
+		assert binary[0:4] == '\x00\x00\x00\x01'
+
+		self.name = binary[175:175+16].replace('\x00', '')	# 16 bytes
 		if self.debug:
 			print self.name
+
+class Nebula:
+	def __init__(self, binary, debug=False):
+		self.debug = debug
+
+		self.type = binary[54:70]
+		self.name = binary[114:130]
+		self.title = binary[334:370]
+
+		if self.debug:
+			print 'nebula type = '+self.type
+			print 'nebula name = '+self.name
+			print 'nebula title = '+self.title
 
 class SAV:
 	def __init__(self, filename=None, debug=False):
@@ -16,7 +33,7 @@ class SAV:
 		self.version		= 'version [1] =\x0d\x0a2053\x0d\x0asaveGameDesc = '
 		self.saveGameDesc	= ''
 		self.binary		= '\x0d\x0abinarySave [1] =\x0d\x0atrue\x0d\x0a'
-		self.untitled		= '\x02\xdd\x60\x00\x10\x00\x00\x00untitled.bzn\x00\x00\x00'
+		self.untitled		= '\x02\xdd\x60\x00\x10\x00\x00\x00untitled.bzn'
 
 		if filename is not None:
 			self.open(filename)
@@ -24,36 +41,62 @@ class SAV:
 	def open(self, filename):
 		self.f = open(filename)
 
-		assert self.f.read(36) == self.version	# 0x000 - 0x023
+		# 0x0000 - 0x0023: version[1] = 2053
 
-		d = self.f.read(256)			# 0x024 - 0x123
+		assert self.f.read(36) == self.version
+
+		# 0x0024 - 0x0123: saveGameDesc
+
+		d = self.f.read(256)
 		self.saveGameDesc = ''.join([chr(int(d[i*2]+d[i*2+1], 16)) for i in range(128)]).strip()
 
 		if self.debug:
 			print self.saveGameDesc
 
-		assert self.f.read(26) == self.binary	# 0x124 - 0x13D
+		# 0x0124 - 0x013D: binarySave[1] = true
 
-		assert self.f.read(23) == self.untitled	# 0x13E - 0x155
+		assert self.f.read(26) == self.binary
 
-		for i in range(5):			# 0x156 - 0x193
-			self.f.read(12)
+		# 0x013E - 0x0151: untitled.bzn
 
-		# 0x194 - 0x7e4: player info
+		assert self.f.read(20) == self.untitled
+
+		# 0x0152: ??
+
+		for i in range(2):
+			self.f.read(5)
+			self.f.read(4)
+
+		for i in range(3):
+			self.f.read(8)
+			self.f.read(4)
+
+		self.f.read(5)
+		self.f.read(4)
+
+		# 0x0191: player setup
 		# 8 players, 202 byte per player
 
 		self.player = []
 		for i in range(8):
 			self.player.append( Player(self.f.read(202), debug=self.debug) )
 
-		
+		# 0x07e1 - 0x0a29: game setup
 
-		# 0x00140b - 0x01d90e
-		# nebulae, varying counts, 827 bytes per nebula
-		# 
+		self.f.read(584)
+
+		# 0x0a2a - 0x140a
+		# 3x klingon turrent ??
+		# 843 bytes per turret
+
+		for i in range(3):
+			self.f.read(843)
+
+		# 0x00140b - 0x01d90e: nebulae
+		# varying counts, 827 bytes per nebula
 
 		self.nebulae = []
-		for i in range(140): # don't know the exact number
+		for i in range(18): # don't know the exact number
 			self.nebulae.append( Nebula(self.f.read(827), debug=self.debug) )
 
 		# moons
